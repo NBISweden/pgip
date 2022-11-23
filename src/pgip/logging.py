f2a5d815712738da6c8fb0e2933ab096cfb9f92c
Per Unneberg <per.unneberg@scilifelab.se>
2022-11-23 22:49:19 +0100

    Move pgip code and data to separate repos
    Minimize size of website repo

--- src/pgip/logging.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-
-def setup_logging(debug=False):
-    logging.basicConfig(
-        level=logging.INFO, format="%(levelname)s [%(name)s:%(funcName)s]: %(message)s"
-    )
-    if debug:
-        logging.getLogger().setLevel(logging.DEBUG)
