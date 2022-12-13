a807b6750fd92cc62fed2f4819da2c62b3534d50
Per Unneberg <per.unneberg@scilifelab.se>
2022-12-13 12:51:29 +0100

    Add images to variant filtering

--- scripts/prepare.py
+++ scripts/prepare.py
@@ -4,6 +4,9 @@ from pathlib import Path
 if not os.getenv("QUARTO_PROJECT_RENDER_ALL"):
     exit()
 
+print(os.getenv("QUARTO_PROJECT_RENDER_ALL"))
+print(os.getenv("QUARTO_PROJECT_OUTPUT_DIR"))
+
 outdir = Path(os.getenv("QUARTO_PROJECT_OUTPUT_DIR"))
 os.system(f"rsync -av img {outdir}")
 os.system(
