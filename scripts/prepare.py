b9c3ecddd434d1c48a12cfd8193fb86f81236921
Per Unneberg <per.unneberg@scilifelab.se>
2022-12-15 21:11:27 +0100

    Reorganize exercises E120 and E130

--- scripts/prepare.py
+++ scripts/prepare.py
@@ -4,12 +4,13 @@ from pathlib import Path
 if not os.getenv("QUARTO_PROJECT_RENDER_ALL"):
     exit()
 
-print(os.getenv("QUARTO_PROJECT_RENDER_ALL"))
-print(os.getenv("QUARTO_PROJECT_OUTPUT_DIR"))
 
 outdir = Path(os.getenv("QUARTO_PROJECT_OUTPUT_DIR"))
 os.system(f"rsync -av img {outdir}")
 os.system(
     'rsync -av --include "*/" --include "*gz" '
     f'--exclude "*.trees" --exclude "*" data/results {outdir}'
+)
+os.system(
+    'rsync -av --include "*/" --include "*.slim" ' f'--exclude "*" recipes {outdir}'
 )
